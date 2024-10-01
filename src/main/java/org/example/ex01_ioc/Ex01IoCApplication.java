037abbfaeb15ffc4d4650d9ae261de93d7d2867b
vinivin153 <vinivin153@naver.com>
2024-10-01 10:51:17 +0900

    Initial commit

---
+++ src/main/java/org/example/ex01_ioc/Ex01IoCApplication.java
@@ -0,0 +1,13 @@
+package org.example.ex01_ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01IoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01IoCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ex01_ioc/Ex01IoCApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ex01_ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01IoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
